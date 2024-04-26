2175b2365fec2fee4b8a15c83a6c4b24f6337acb
Farheen00 <159334516+Farheen00@users.noreply.github.com>
2024-04-26 16:51:30 +0500

    Update main.js

--- Album/main.js
+++ Album/main.js
@@ -1,16 +1,11 @@
 //Album
-// function make_album(artistName:string , albumTitle:string , numberOfTracks?:number){
-//     {return(`${artistName} ,${albumTitle},${numberOfTracks}`)}
-// }
-// let album1 = make_album("Hadiqa" , "Heartbeat");
-// let album2 = make_album("Ali" , "Sufiyana");
-// let album3 = make_album("Atif" , "Lovely Tunes");
-// console.log(album1);
-// console.log(album2);
-// console.log(album3);
-// let album4 = make_album("Fawad" ,"Ehsaas" , 7);
-// console.log(album4);
-function make_album(artistName, albumTitle, numberOfTracks) {
+interface musicAlbum{
+        artistName:string ,
+         albumTitle:string ,
+          numberOfTracks?:number
+}
+            
+function make_album(artistName, albumTitle, numberOfTracks?):musicAlbum {
     return { artistName: artistName, albumTitle: albumTitle, numberOfTracks: numberOfTracks
     };
 }
