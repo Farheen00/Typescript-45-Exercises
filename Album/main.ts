7704ef80af1414b68829767d1c20e148302e57d2
Farheen00 <159334516+Farheen00@users.noreply.github.com>
2024-04-26 16:42:53 +0500

    Update main.ts

--- Album/main.ts
+++ Album/main.ts
@@ -1,15 +1,4 @@
 //Album
-// function make_album(artistName:string , albumTitle:string , numberOfTracks?:number){
-//     {return(`${artistName} ,${albumTitle},${numberOfTracks}`)}
-// }
-// let album1 = make_album("Hadiqa" , "Heartbeat");
-// let album2 = make_album("Ali" , "Sufiyana");
-// let album3 = make_album("Atif" , "Lovely Tunes");
-// console.log(album1);
-// console.log(album2);
-// console.log(album3);
-// let album4 = make_album("Fawad" ,"Ehsaas" , 7);
-// console.log(album4);
 
 interface musicAlbum{
         artistName:string ,
